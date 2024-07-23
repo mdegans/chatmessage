a34062ff1a67428204279e554daf645a95763fa3
Michael de Gans <michael.john.degans@gmail.com>
2024-07-23 16:40:29 -0700

    add some useful chat related methods to `Session`

--- src/session.rs
+++ src/session.rs
@@ -1,4 +1,8 @@
-use crate::{chat, message::Message, user};
+use crate::{
+    chat::{self, Chat},
+    message::Message,
+    user,
+};
 #[cfg(feature = "client")]
 use serde::Deserialize;
 #[cfg(feature = "server")]
@@ -12,4 +16,20 @@ pub struct Session {
     pub user: user::Info,
     pub active_chat: Option<crate::chat::Id>,
     pub chat_list: crate::chat::List,
+}
+
+impl Session {
+    pub fn active_chat(&self) -> Option<&Chat> {
+        self.active_chat
+            .as_ref()
+            .and_then(|id| self.chat_list.chats.get(id))
+    }
+
+    pub fn chats(&self) -> impl Iterator<Item = &Chat> {
+        self.chat_list.chats.values()
+    }
+
+    pub fn chat_titles(&self) -> impl Iterator<Item = &str> {
+        self.chats().map(|chat| chat.title.as_str())
+    }
 }
